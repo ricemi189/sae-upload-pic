b85044795538266734c29a94ad06ae5813035141
axiaoxin <254606826@qq.com>
2014-04-10 12:09:01 +0000

    add logo on pic

--- vp104dpIc/uploadpic/views.py
+++ vp104dpIc/uploadpic/views.py
@@ -1,7 +1,16 @@
+i# -*- coding:utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 import time
+import os
+try:
+    import Image
+    import ImageDraw
+    import ImageFont
+except:
+    from PIL import Image, ImageDraw, ImageFont
+import StringIO
 
 
 @csrf_exempt
@@ -17,5 +26,24 @@ def home(request):
         pic_name = str(time.time()) + '.' + pic_suffix
         ob = sae.storage.Object(pic.read())
         url = s.put(domain_name, pic_name, ob)
+
+        bucket = sae.storage.Bucket(domain_name)
+        content = bucket.get_object_contents(pic_name)
+        buf = StringIO.StringIO(content)
+        img = Image.open(buf)
+        buf2 = StringIO.StringIO()
+        draw = ImageDraw.Draw(img)
+        x,y = img.size
+        font_size = 18
+        font = ImageFont.truetype(
+            os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                     'font.ttf'),
+            font_size)
+        logo = u'@阿小信的博客'
+        draw.text((x/2, y/2), logo, font=font)
+        img.save(buf2, 'png')
+        bucket.put_object(pic_name, buf2.getvalue())
+        buf2.close()
+        buf.close()
         return render_to_response('home.html', {'pic_url': url})
     return render_to_response('home.html')
